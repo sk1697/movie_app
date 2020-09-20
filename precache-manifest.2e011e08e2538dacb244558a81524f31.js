90677f744a019d19a618199c9dc9ccc990f4dfc0
Junghyun Park <sk1697@naver.com>
2020-09-20 14:30:00 +0900

    Updates

---
+++ precache-manifest.2e011e08e2538dacb244558a81524f31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cfa0e32feceeecdb6172653dbd963e3",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "adc599a0df12c11df069",
+    "url": "/movie_app/static/css/main.ccfe9246.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc599a0df12c11df069",
+    "url": "/movie_app/static/js/main.f8fa62dc.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
